0b1d7a008b7872d3b074516ce97f872a15a0d771
Aryan Singh <aquiveal@gmail.com>
2026-07-17 21:21:47 +0530

    refactor: simplify todo update logic
    Remove provider-specific execution queueing and use native
    implementation directly. This eliminates unnecessary complexity
    and the need to fetch the playbook document and generate
    execution hashes.

--- frappe_playbook/playbook/doctype/todo/todo.py
+++ frappe_playbook/playbook/doctype/todo/todo.py
@@ -1,16 +1,7 @@
 import frappe
-from frappe_playbook.playbook.doctype.playbook_provider.playbook_provider import get_provider_instance
 from frappe_playbook.playbook.doctype.playbook_execution.playbook_execution import queue_resume_execution as native_queue_resume_execution
 
 def on_update(doc, method):
     if doc.status == "Closed" and doc.has_value_changed("status") and doc.playbook_execution:
         execution_doc = frappe.get_doc("Playbook Execution", doc.playbook_execution)
-        playbook_doc = frappe.get_doc("Playbook", execution_doc.playbook)
-        
-        execution_name = frappe.generate_hash(f"{doc.name}-{doc.modified}", length=10)
-        
-        if playbook_doc.provider:
-            provider = get_provider_instance(playbook_doc.provider)
-            provider.queue_resume_execution(execution_doc, doc.response_body, doc.callback_url, execution_name)
-        else:
-            native_queue_resume_execution(execution_doc, doc.response_body)
+        native_queue_resume_execution(execution_doc, doc.response_body)
